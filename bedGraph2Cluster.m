f298f9bc862297488551a5c5c7345c1b7ed0eff6
hanjunlee21 <67846757+hanjunlee21@users.noreply.github.com>
2022-08-19 16:36:27 -0400

    Add files via upload

--- bedGraph2Cluster.m
+++ bedGraph2Cluster.m
@@ -925,3 +925,16 @@ if isstruct(S)
     end
 end
 end
+
+function S2 = keep_fields(S,flds)
+if ischar(flds), flds = {flds}; end
+S2=[];
+for i=1:length(flds)
+  if isempty(S)
+    f = [];
+  else
+    f = getfield(S,flds{i});
+  end
+  S2=setfield(S2,flds{i},f);
+end
+end
